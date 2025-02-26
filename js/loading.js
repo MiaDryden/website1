5d2c7e27a6c8a310025e39adb7f48bd99e07b85a
MiaDryden <drydenmg@gmail.com>
2025-02-26 17:22:06 +0000

    Wed, Feb 26, 2025, 9:22 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,4 +3,9 @@ const bg =document.querySelector('.bg')
 
 let load = 0
 
-loadText.innerText = `${load}%`
+let int = setInterval(blurring, 300)
+
+function blurring() {
+    load = load + 1
+    loadText.innerText = `${load}%`
+}
