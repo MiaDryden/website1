7a464f8fa49f4984f240ee81955ce8a74cf8a9fc
MiaDryden <drydenmg@gmail.com>
2025-02-26 17:23:30 +0000

    Wed, Feb 26, 2025, 9:23 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -7,5 +7,10 @@ let int = setInterval(blurring, 300)
 
 function blurring() {
     load = load + 1
+    if (load > 99){
+        clearInterval(int)
+    }
+
     loadText.innerText = `${load}%`
+    
 }
