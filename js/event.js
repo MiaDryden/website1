0dcde03b0f11a20c59627cbefd01a867c3e7f939
MiaDryden <drydenmg@gmail.com>
2025-03-04 18:40:45 +0000

    Tue, Mar 4, 2025, 10:40 AM -08:00

--- js/event.js
+++ js/event.js
@@ -1,6 +1,8 @@
 const element1 = document.getElementById('element1')
 const element2 = document.getElementById('element2')
 const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
+
 element1.addEventListener('click', ()=>{
     element1.innerHTML = "I've been clicked! <br> Try double clicking me."
 })
@@ -21,3 +23,50 @@ element2.addEventListener('mouseout', ()=>{
     element2.style.backgroundColor = "#BB33BB"
 })
 
+let positionY = 0;
+let positionX = 0;
+
+document.addEventListener('keyup', (event)=>{
+    console.log(event.key)
+
+    if (event.key == "ArrowDown') {
+        positionY = positionY + 10
+        }
+        else if (event.key == 'ArrowUp') {
+            positionY = positionY - 10
+        }
+        else if (event.key == 'ArrowLeft') {
+            positionX = positionX + 10
+        }
+        else if (event.key == 'ArrowRight') {
+                positionX = positionX - 10
+        }
+        element3.style.top = '${positionY}px'
+        element3.style.right = '${positionX}px'
+
+})
+
+element3.addEventListener('click', ()=> {
+    element4.style.width = "10px"
+    element4.style.height = "10px"
+    element4.innerHTML = ""
+    element4.style.opacity = .3
+})
+
+document.addEventListener('keyup', (event)=>{
+    if (event.key == "j") {
+        element4.style.width = "300px"
+        element4.style.height = "50px"
+        element4.innerHTML = " <h4>Click me to make me
+        small. <br> Press 'j' to bring me back. </h4>"
+        element4.style.opacity = 1
+
+    }
+})
+
+const list = ['running', 'paused']
+let index = 0
+element5.addEventListener('click', ()=>{
+    element5.style.animationPlayState = list[index]
+    index = (index + 1) % 2
+})
