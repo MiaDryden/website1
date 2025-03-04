48df21daf0dfbe470b5184edaa569ca0cbbafb35
MiaDryden <drydenmg@gmail.com>
2025-03-04 02:24:56 +0000

    Mon, Mar 3, 2025, 6:24 PM -08:00

---
+++ js/event.js
@@ -0,0 +1,23 @@
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+element1.addEventListener('click', ()=>{
+    element1.innerHTML = "I've been clicked! <br> Try double clicking me."
+})
+
+element1.addEventListener('dbt click',()=>{
+    element1.innerHTML = "I've been double clicked! <br>
+    Try single clicking me."
+})
+
+
+element2.addEventListener('mouseover',()=>{
+    element2.style.color = "pink"
+    element2.style.backgroundColor = "#blue"
+})
+
+element2.addEventListener('mouseout', ()=>{
+    element2.style.color = "white"
+    element2.style.backgroundColor = "#BB33BB"
+})
+
