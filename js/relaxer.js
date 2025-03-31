562c5bfa8cbbfa3c07afa064b4bd4657c29cb521
MiaDryden <drydenmg@gmail.com>
2025-03-31 16:55:08 +0000

    Mon, Mar 31, 2025, 9:55 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,2 +1,7 @@
-const container =
-const txt = 
+const container = document.getElementById('container')
+const txt = document.getElementById('text')
+
+function breatheAnimation() {
+    text.innerHTML = 'Breathe In'
+    container.classList.add('grow')
+}
