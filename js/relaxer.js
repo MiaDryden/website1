b3aef3ba63ae267b2005a32a133f28aa3b56c7e8
MiaDryden <drydenmg@gmail.com>
2025-03-31 17:00:19 +0000

    Mon, Mar 31, 2025, 10:00 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,7 +1,20 @@
 const container = document.getElementById('container')
 const txt = document.getElementById('text')
 
+breatheAnimation()
+breatheAnimation()
 function breatheAnimation() {
     text.innerHTML = 'Breathe In'
+    container.classList.remove('shrink')
     container.classList.add('grow')
+
+    setTimeout( ()=> {
+        text.innerHTMl = "Hold'
+    }, 3000)
+
+    setTimeout(() => {
+        text.innerHTML = 'Breathe Out!'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
 }
