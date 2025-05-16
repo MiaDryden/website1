a3a02da104d8adf2b7699b21bb40f198abedbb6a
MiaDryden <drydenmg@gmail.com>
2025-05-16 17:03:40 +0000

    Fri, May 16, 2025, 10:03 AM -07:00

---
+++ js/speech.js
@@ -0,0 +1,8 @@
+const main = document.querySelecotr('main')
+const voiceSelect = document.getElementById('voices')
+const testarea = document.getElementById('text')
+const readBtn = document.getElementById('read')
+const toggleBtn = document.getElementById('toggle')
+const closeBtn = document.getElementById('close')
+
+
