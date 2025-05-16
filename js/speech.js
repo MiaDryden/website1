bb1d51ec6c61195abd78ea3fb669a4aa201bd0af
MiaDryden <drydenmg@gmail.com>
2025-05-16 17:12:07 +0000

    Fri, May 16, 2025, 10:12 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -5,4 +5,53 @@ const readBtn = document.getElementById('read')
 const toggleBtn = document.getElementById('toggle')
 const closeBtn = document.getElementById('close')
 
-
+const data = [
+    {
+        image: '../drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: '../food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: '../tired.jpg',
+        text: "I'm tired"
+    },
+    {
+        image: '../hurt.jpg',
+        text: "I'm hurt"
+    },
+    {
+        image: '../happy.jpg',
+        text: "I'm happy"
+    },
+    {
+        image: '../angry.jpg',
+        text: "I'm angry"
+    },
+    {
+        image: '../sad.jpg',
+        text: "I'm sad"
+    },
+    {
+        image: '../scared.jpg',
+        text: "I'm scared"
+    },
+    {
+        image: '../outside.jpg',
+        text: "I want to go outside"
+    },
+    {
+        image: '../home.jpg',
+        text: "I want to go home"
+    },
+    {
+        image: '../school.jpg',
+        text: "I want to go to school"
+    },
+    {
+        image: '../grandma.jpg',
+        text: "I want to go to Grandma's"
+    },
+]
