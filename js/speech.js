096173fee809b1a8fb4a2816d37af45402315190
MiaDryden <drydenmg@gmail.com>
2025-05-22 17:09:41 +0000

    Thu, May 22, 2025, 10:09 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -52,7 +52,7 @@ const data = [
     },
     {
         image: '../grandma.jpg',
-        text: "I want to go to Grandma's"
+        text: "I want to go to Grandmas"
     },
 ]
 
@@ -66,5 +66,74 @@ function createBox(item) {
     <img src="${item.image}" alt = "${item.text}" />
     <p class="info"> ${item.text} </p>
     `
+
+    box.addEventListener('click',() => {
+        setTextMessage(item.text)
+        speakText()
+
+        // Add active effect
+        box.classList.add('active')
+        setTimeout( () => box.classList.remove('active'), 800)
+    })
+
     main.appendChild(box)
 }
+
+//Initialize Speech Sythesis
+const message = newSpeechSynthesisUtterance()
+
+//Set Text
+function setTextMessage(text) {
+    message.text = text
+}
+
+//Speak Test
+function speakText() {
+    speechSynthesis.speak(message)
+}
+
+
+// Toggle Text Box
+toggleBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.toggle('show')
+})
+
+// Close Box
+closeBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.remove('show')
+})
+
+let voices = []
+
+function getVoices() {
+    voices = speechSynthesis.getVoices()
+
+    voices.forEach(voice => {
+        const option = document.createElement('option')
+
+        option.value = voice.name
+        option.innerText = `${voice.name}${voice.lang}`
+
+        voicesSelect.appendChild(option)
+    })
+}
+
+// voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+
+
+//Change Voice
+voicesSelect.addEventListener('change',setVoice)
+
+//Set Voice
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value)
+}
+
+getVoices()
+
+// Read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage("textarea.value")
+        speakText()
+})
